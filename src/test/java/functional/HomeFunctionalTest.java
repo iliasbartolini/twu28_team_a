0d6d51962f52feb9f4ba7eef814cbc6b8ec4edff
Ilias Bartolini <iliasbartolini@gmail.com>
2012-08-19 20:03:13 +0530

    [Ilias,SW] added functional tests with selenium

---
+++ src/test/java/functional/HomeFunctionalTest.java
@@ -0,0 +1,25 @@
+package functional;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class HomeFunctionalTest {
+
+    @Test
+    public void shouldFail() throws Exception {
+        WebDriver webDriver = new FirefoxDriver();
+        webDriver.get("http://localhost:9876/twu");
+        WebElement link = webDriver.findElement(By.tagName("a"));
+
+        assertThat( link.getText(), is("Try me"));
+
+        webDriver.close();
+    }
+
+}
